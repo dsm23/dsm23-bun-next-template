ea9b07ff530a56d276a81d7a5bced0208db8541e
David Murdoch <dsmurdoch357@gmail.com>
2025-11-03 14:16:10 +0000

    chore(eslint): update eslint config
    * remove @eslint/compat

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,36 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-
-// import next from "@next/eslint-plugin-next";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { includeIgnoreFile } from "@eslint/compat";
-import { FlatCompat } from "@eslint/eslintrc";
+import next from "@next/eslint-plugin-next";
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import * as mdx from "eslint-plugin-mdx";
 import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import storybook from "eslint-plugin-storybook";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
-import ts from "typescript-eslint";
+import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, ".gitignore");
-
-const compat = new FlatCompat();
-
-const compatConfig = compat.config({
-  extends: [
-    // https://github.com/vercel/next.js/discussions/49337
-    "plugin:@next/eslint-plugin-next/core-web-vitals",
-
-    // https://github.com/facebook/react/issues/28313
-    "plugin:react-hooks/recommended",
-  ],
-});
-
-export default ts.config(
-  includeIgnoreFile(gitignorePath),
+export default defineConfig(
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "coverage/**",
+    "storybook-static/**",
+    "playwright-tests/**",
+    "test-results/**",
+  ]),
+  prettier,
   {
-    files: ["**/*.{js,mjs,cjs,ts,md,mdx,jsx,tsx}"],
+    files: ["**/src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs.flat["recommended-latest"],
+      next.flatConfig.coreWebVitals,
+      storybook.configs["flat/recommended"],
+    ],
     languageOptions: {
       globals: globals.nodeBuiltin,
       parserOptions: {
@@ -41,56 +39,103 @@ export default ts.config(
       },
     },
   },
-  js.configs.recommended,
-  ...ts.configs.strictTypeChecked,
-  ...ts.configs.stylisticTypeChecked,
-  react.configs.flat["jsx-runtime"],
-  prettier,
-  ...compatConfig,
   {
-    files: ["**/*.{js,md,mdx,mjs,ts,tsx}"],
-    rules: { "@next/next/no-duplicate-head": "off" },
+    files: ["**/*.{jsx,mdx,tsx}"],
+    extends: [
+      react.configs.flat.recommended,
+      react.configs.flat["jsx-runtime"],
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigDirName: import.meta.dirname,
+      },
+    },
+    rules: {
+      "react/function-component-definition": [
+        "error",
+        {
+          namedComponents: "arrow-function",
+          unnamedComponents: "arrow-function",
+        },
+      ],
+    },
     settings: { react: { version: "detect" } },
   },
-  { files: ["!**/src/**"], ...ts.configs.disableTypeChecked },
   {
     files: ["**/src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/consistent-type-imports": [
         "error",
-        { prefer: "type-imports" },
+        {
+          prefer: "type-imports",
+        },
       ],
       "@typescript-eslint/no-confusing-void-expression": "off",
       "@typescript-eslint/no-empty-function": "off",
       "@typescript-eslint/no-misused-promises": [
         "error",
-        { checksVoidReturn: false },
+        {
+          checksVoidReturn: false,
+        },
       ],
       "@typescript-eslint/non-nullable-type-assertion-style": "off",
       "@typescript-eslint/restrict-template-expressions": [
         "error",
-        { allowNumber: true, allowBoolean: true },
+        {
+          allowNumber: true,
+          allowBoolean: true,
+        },
       ],
       "@typescript-eslint/triple-slash-reference": [
         "error",
-        { types: "prefer-import" },
+        {
+          types: "prefer-import",
+        },
       ],
       "no-console": [
         "error",
-        { allow: ["debug", "error", "info", "trace", "warn"] },
+        {
+          allow: ["debug", "error", "info", "trace", "warn"],
+        },
       ],
-      "tailwindcss/no-custom-classname": "off",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector:
+            "ImportDeclaration[source.value='react'][specifiers.0.type='ImportDefaultSpecifier']",
+          message:
+            "Default React import not allowed since we use the TypeScript jsx-transform. If you need a global type that collides with a React named export (such as `MouseEvent`), try using `globalThis.MouseHandler`",
+        },
+        {
+          selector:
+            "ImportDeclaration[source.value='react'] :matches(ImportNamespaceSpecifier)",
+          message:
+            "Named * React import is not allowed. Please import what you need from React with Named Imports",
+        },
+      ],
     },
   },
   {
-    // Configure.mdx
-    files: ["**/*.mdx"],
-    rules: {
-      "react/jsx-uses-vars": "error",
-      "tailwindcss/no-custom-classname": "off",
+    files: ["!**/src/**", "*.{js,mjs,cjs,ts,jsx,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat["recommended-latest"],
+      prettier,
+    ],
+    languageOptions: {
+      globals: globals.nodeBuiltin,
+      // parserOptions: {
+      //   projectService: true,
+      //   tsconfigDirName: import.meta.dirname,
+      // },
     },
   },
   mdx.flat,
-  storybook.configs["flat/recommended"],
 );
