15f0cc4db86899163fa0435a175f70be9bdd58a4
David Murdoch <dsmurdoch357@gmail.com>
2025-12-15 01:38:19 +0000

    chore(playwright): separate playwright configs

--- playwright.prod.config.ts
+++ playwright.prod.config.ts
@@ -1,7 +1,8 @@
 import fs from "node:fs";
 import path from "node:path";
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 import dotenv from "dotenv";
+import config from "./playwright.config";
 
 const PORT = process.env.PORT ?? "3000";
 
@@ -28,36 +29,18 @@ injectFromEnvFile();
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./playwright-tests",
-  fullyParallel: true,
-  forbidOnly: Boolean(process.env.CI),
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "blob" : "html",
+  ...config,
   use: {
+    ...config.use,
     baseURL: `http://localhost:${PORT}`,
-
-    trace: "on-first-retry",
   },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
-
+  /* Run your local dev server before starting the tests */
   webServer: {
-    command: "bun run dev",
+    command: `bun run build \\
+        && ln -s "$(pwd)/public" .next/standalone \\
+        && ln -s "$(pwd)/.next/static" .next/standalone/.next \\
+        && HOSTNAME=localhost bun --port=${PORT} .next/standalone/server.js`,
     url: `http://localhost:${PORT}`,
     reuseExistingServer: !process.env.CI,
   },
