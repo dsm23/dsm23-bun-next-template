e758fe537b21d78518f729e33d1e097e43afee85
David Murdoch <dsmurdoch357@gmail.com>
2026-03-26 10:17:33 +0000

    chore(vitest): remove storybook vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
             provider: playwright(),
             instances: [{ browser: "chromium" }],
           },
-          setupFiles: [".storybook/vitest.setup.ts"],
         },
       },
     ],
